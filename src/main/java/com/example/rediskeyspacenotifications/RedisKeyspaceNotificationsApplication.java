b91ece69647c7c221ecdc62e5f928dfc911a0662
Soo <px201226@gmail.com>
2023-07-17 18:42:15 +0900

    initialize commit

---
+++ src/main/java/com/example/rediskeyspacenotifications/RedisKeyspaceNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rediskeyspacenotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisKeyspaceNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisKeyspaceNotificationsApplication.class, args);
+	}
+
+}
